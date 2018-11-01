78c5c6a01ddf5cce5426016396e8618355a104e6
Kingsley Ubani <kingsleyubani@ticketmaster.com>
2018-11-01 03:38:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sup App
+
+This project will build a silly GUI app, similar to the infamous Yo! App. The project will utilize the JavaFx framework.
+
+## About this repository
+
+This repository is used for the [Treehouse](https://www.teamtreehouse.com) workshop [Build a JavaFX Application](https://teamtreehouse.com/library/build-a-javafx-application).
---
+++ src/sample/Main.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        /*Group root = new Group();
+        Text txt = new Text("Sup?");
+        txt.setFont(new Font("Papyrus", 50));
+        Label label = new Label("Name:");
+        TextField nameFld = new TextField();
+        GridPane grid = new GridPane();
+        grid.add(label, 0, 0);
+        grid.add(nameFld, 1, 0);
+        grid.setHgap(20);
+        //grid.setGridLinesVisible(true);
+        Button btn = new Button();
+        grid.add(btn, 1, 1);
+        btn.setText("Say Sup!");
+        btn.setOnAction(evt -> System.out.printf("Sup, %s! %n",
+                nameFld.getText()));
+        txt.setY(50);
+        VBox box = new VBox();
+        box.getChildren().addAll(txt, grid);
+        root.getChildren().add(box);*/
+        primaryStage.setTitle("Sup");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
